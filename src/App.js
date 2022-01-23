867321b3012eb8380b9fc17b6b1621396237bf1f
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-01-23 22:31:52 +0530

    Finished navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import "./App.css"
+import {  BrowserRouter as Router,Route } from "react-router-dom";
 
-const name = "vikas";
+import data from "./data/data.json";
+
+import PreNavBar from "./components/PreNavBar.js";
+import NavBar from "./components/NavBar.js";
+import Slider from "./components/Slider.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <span>
-        My Name
-        {name}
-      </span>
-      <span>
-          is Narendra Kumar Jha
-      </span>
-
-    </div>
+    <Router>
+      <PreNavBar/>
+      <NavBar/>
+      <Slider start={data.banner.start} />
+      <Slider start={data.banner.start}/>
+    </Router>
   );
 }
 
