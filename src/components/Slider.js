517818949ac98875c0bc57d3ca0255f3a47e2480
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-01-26 18:37:33 +0530

    Carousel setup finished

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,34 @@
-import React from 'react';
+
+// import React from 'react'
+// import Carousel from 'react-bootstrap/Carousel'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// const Slider = ({start}) => {
+//     return (
+//         <Carousel fade>
+       
+
+//        {start.map((item)=>(
+//             <Carousel.Item>
+//             <img
+//               className="d-block w-100"
+//               src={item}
+//               alt="First slide"
+//             />
+//           </Carousel.Item>
+//        ))}
+       
+//       </Carousel>
+//     )
+// }
+
+// export default Slider
+
+import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Slider = ({start}) => {
     return (
@@ -21,5 +49,4 @@ const Slider = ({start}) => {
     )
 }
 
-
-export default Slider;
+export default Slider
